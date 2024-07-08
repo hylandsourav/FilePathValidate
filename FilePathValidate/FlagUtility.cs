4cf269067d3decfffe9b7749da400826ab4532ce
Sourav Maji <Sourav.Maji@hyland.com>
2024-07-09 03:05:42 +0530

    Add project files.

---
+++ FilePathValidate/FlagUtility.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilePathValidate
+{
+    public static class FlagUtility
+    {
+        /// <summary>
+        /// Returns true if all flags are set on the specified bit field.
+        /// Other flags are ignored
+        /// </summary>
+        /// <typeparam name="T">Enumeration with Flags attribute</typeparam>
+        /// <param name="bitField">Flagged variable</param>
+        /// <param name="flag">Flag to test</param>
+        /// <returns></returns>
+        public static bool IsFlagSet<T>(long bitField, T flag)
+            where T : struct, IConvertible
+            => IsFlagSet(bitField, flag.ToInt64(null));
+
+        /// <summary>
+        /// Returns true if all flags are set on the specified bit field.
+        /// Other flags are ignored
+        /// </summary>
+        /// <typeparam name="T">Enumeration with Flags attribute</typeparam>
+        /// <param name="bitField">Flagged variable</param>
+        /// <param name="flag">Flag to test</param>
+        /// <returns></returns>
+        public static bool IsFlagSet<T>(ulong bitField, T flag)
+            where T : struct, IConvertible
+            => IsFlagSet(bitField, flag.ToUInt64(null));
+
+        /// <summary>
+        /// Returns true if all flags are set on the specified bit field.
+        /// Other flags are ignored 
+        /// </summary>
+        /// <param name="bitField">Flagged variable</param>
+        /// <param name="flag">Flag to test</param>
+        /// <returns></returns>
+        public static bool IsFlagSet(ulong bitField, ulong flag) => ((bitField & flag) == flag);
+
+        /// <summary>
+        /// Returns true if all flags are set on the specified bit field.
+        /// Other flags are ignored
+        /// </summary>
+        /// <typeparam name="T">Enumeration with Flags attribute</typeparam>
+        /// <param name="bitField">Flagged variable</param>
+        /// <param name="flag">Flag to test</param>
+        /// <returns></returns>
+        public static bool IsFlagSet<T>(T bitField, T flag)
+            where T : struct, IConvertible
+            => IsFlagSet(Convert.ToInt64(bitField), Convert.ToInt64(flag));
+
+        /// <summary>
+        /// Convenience method that will return true if any of the provided flags are set on the bit field.
+        /// </summary>
+        /// <typeparam name="T">Enumeration that is based on bit fields</typeparam>
+        /// <param name="bitField">The value to check</param>
+        /// <param name="flags">The flag values to check</param>
+        /// <returns>True if any are set; false if none are set.</returns>
+        public static bool IsAnyFlagSet<T>(T bitField, params T[] flags)
+             where T : struct, IConvertible
+        {
+            foreach (T flag in flags)
+            {
+                if (IsFlagSet(bitField, flag))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if the flag is set on the specified bit field.
+        /// </summary>
+        /// <typeparam name="T">Enumeration with Flags attribute</typeparam>
+        /// <param name="t">Flagged bitfield array All flags must have the MutliFlagAttribute set ie.. [MultiFlag, 0x0004000, 0]</param>
+        /// <param name="bitFields">Flag to test</param>
+        /// <returns></returns>
+        public static bool IsMultiFlagSet<T>(T t, long[] bitFields)
+            where T : struct, IConvertible
+        {
+            MultiFlagAttribute att = GetMultiFlag(t);
+            return IsFlagSet(bitFields[att.Column], att.FlagValue);
+        }
+
+        /// <summary>
+        /// Extracts the MultiFlagAttibute from a tagged enum value;
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <returns>MultiFlagAttibute</returns>
+        public static MultiFlagAttribute GetMultiFlag<T>(T t)
+        {
+            FieldInfo fi = typeof(T).GetField(t.ToString());
+            Object[] att = fi.GetCustomAttributes(true);
+            return (MultiFlagAttribute)att[0];
+        }
+
+        /// <summary>
+        /// Returns true if all flags are set on the specified bit field.
+        /// Other flags are ignored
+        /// </summary>
+        /// <param name="bitField">Flagged variable</param>
+        /// <param name="flag">Flag to test</param>
+        /// <returns></returns>
+        public static bool IsFlagSet(long bitField, long flag) => ((bitField & flag) == flag);
+
+        /// <summary>
+        /// Sets a bit-field to either on or off for the specified flag.
+        /// </summary>
+        /// <typeparam name="T">Enumeration with Flags attribute</typeparam>
+        /// <param name="bitField">Long (int32) variable to set the flagged value on</param>
+        /// <param name="flag">Flagged Enum Value to add/remove from the bitField</param>
+        /// <param name="on">Should this flag be added or removed</param>
+        public static void SetFlag<T>(ref long bitField, T flag, bool on)
+            where T : struct, IConvertible
+        {
+            bitField = SetFlag(bitField, flag, on);
+        }
+
+        /// <summary>
+        /// Sets a bit-field to either on or off for the specified flag.
+        /// </summary>
+        /// <typeparam name="T">Enumeration with Flags attribute</typeparam>
+        /// <param name="bitField">Flagged variable</param>
+        /// <param name="flag">Flag to change</param>
+        /// <param name="on">bool</param>
+        /// <returns>The flagged variable with the flag changed</returns>
+        public static long SetFlag<T>(long bitField, T flag, bool on)
+            where T : struct, IConvertible
+        {
+            if (on)
+            {
+                return bitField | flag.ToInt64(null);
+            }
+            else
+            {
+                return bitField & (~flag.ToInt64(null));
+            }
+        }
+
+        /// <summary>
+        /// Sets a bit-field to either on or off for the specified flag.
+        /// </summary>
+        /// <typeparam name="T">Enumeration with Flags attribute</typeparam>
+        /// <param name="bitField">Flagged variable</param>
+        /// <param name="flag">Flag to change</param>
+        /// <param name="on">bool</param>
+        /// <returns>The flagged variable with the flag changed</returns>
+        public static long SetFlag<T>(T bitField, T flag, bool on)
+            where T : struct, IConvertible
+        {
+            return SetFlag(bitField.ToInt64(null), flag, on);
+        }
+    }
+    public sealed class MultiFlagAttribute : Attribute
+    {
+        private long _column;
+        private long _flagValue;
+        public MultiFlagAttribute(long flagValue, long column)
+        {
+            this._flagValue = flagValue;
+            this._column = column;
+        }
+
+        public long Column
+        {
+            get
+            {
+                return this._column;
+            }
+        }
+
+        public long FlagValue
+        {
+            get
+            {
+                return this._flagValue;
+            }
+        }
+    }
+}
---
+++ FilePathValidate/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FilePathValidate;
+
+public static class FileAndPathNameIllegalCharUtility
+{
+    ////OnBase defined illegal characters and replacements
+    private static readonly Dictionary<char, char> OB_SWAP_CHARS = new Dictionary<char, char>{
+                {'\\', '-'},
+                {'/', '-'},
+                {':', ';'},
+                {'*', '+'},
+                {'?', '!'},
+                {'\"', '\''},
+                {'<', '['},
+                {'>', ']'},
+                {'|', '!'},
+            };
+
+
+
+    //URI illegal characters.
+    private static readonly char[] URI_ILLEGAL_CHARS = { '@', '"', '$', '&', ':', '<', '>', '{', '}', '[', ']', '#', '%', '/', ';', '=', '?', '\\', '^', '|', '~', '\'', '\"' };
+
+
+    /// <summary>
+    /// Remove illegal characters from a file name or any string.
+    /// </summary>
+    /// <param name="fileNameToScrub"></param>
+    /// <param name="fileName"></param>
+    /// <param name="scrubOptions"></param>
+    /// <returns></returns>
+    static public string CreateSafeFileName(string fileNameToScrub, out string fileName, IllegalCharacterScrubOptions scrubOptions)
+    {
+
+        string _tempFileName = fileNameToScrub;
+
+        if (FlagUtility.IsFlagSet(scrubOptions, IllegalCharacterScrubOptions.ScrubOBIllegalChars))
+        {
+            _tempFileName = ReplaceOBReservedCharacters(fileNameToScrub);
+        }
+        if (FlagUtility.IsFlagSet(scrubOptions, IllegalCharacterScrubOptions.ScrubWindowsIllegalChars))
+        {
+            _tempFileName = ReplaceWindowsIllegalCharacters(_tempFileName);
+        }
+        if (FlagUtility.IsFlagSet(scrubOptions, IllegalCharacterScrubOptions.ScrubURIIllegalCharacters))
+        {
+            _tempFileName = ReplaceURIIllegalCharacters(_tempFileName);
+        }
+
+        fileName = _tempFileName;
+
+        return fileName;
+    }
+
+    static public string CreateSafeFilePath(string filePathToScrub)
+    {
+        if (string.IsNullOrEmpty(filePathToScrub))
+        {
+            throw new ArgumentNullException("The provided file path was null or empty.");
+        }
+
+        // Separate the file path and filename
+        string directory = Path.GetDirectoryName(filePathToScrub);
+        string filename = Path.GetFileName(filePathToScrub);
+        
+        // Ensure directory and filename are not null
+        if (string.IsNullOrEmpty(directory) || string.IsNullOrEmpty(filename))
+        {
+            throw new ArgumentException("The provided file path is not valid.");
+        }
+
+        // Replace invalid characters in the file path
+        string canonicalDirectory = ReplaceInvalidCharactersFromFilePath(directory);
+
+        // Replace URI illegal characters in the filename
+        string sanitizedFileName = CreateSafeFileName(filename, out string safeFileName, IllegalCharacterScrubOptions.All);
+        
+        string sanitizedFilePath = Path.Combine(canonicalDirectory, sanitizedFileName);
+        
+        // Ensure there are no path traversal sequences
+        if (ContainsPathTraversalSequence(sanitizedFilePath))
+        {
+            throw new ArgumentException("The provided file path contains path traversal sequences.");
+        }
+
+        return sanitizedFilePath;
+    }
+
+
+    static private string ReplaceOBReservedCharacters(string nameToScrub)
+    {
+        StringBuilder safeName = new StringBuilder(nameToScrub);
+
+        foreach (char c in OB_SWAP_CHARS.Keys)
+        {
+            safeName.Replace(c, OB_SWAP_CHARS[c]);
+        }
+        return safeName.ToString();
+    }
+
+    static private string ReplaceWindowsIllegalCharacters(string nameToScrub)
+    {
+        string safeName = nameToScrub;
+        int i;
+        i = safeName.IndexOfAny(Path.GetInvalidPathChars());
+        while (i != -1)
+        {
+            char toRemoveChar = safeName[i];
+            safeName = safeName.Replace(toRemoveChar, '_');
+            i = safeName.IndexOfAny(Path.GetInvalidPathChars(), i);
+        }
+        return safeName;
+    }
+
+    static private string ReplaceURIIllegalCharacters(string nameToScrub)
+    {
+        string safeName = nameToScrub;
+
+        int i = -1;
+        i = safeName.IndexOfAny(URI_ILLEGAL_CHARS);
+        while (i != -1)
+        {
+            char toRemoveChar = safeName[i];
+            safeName = safeName.Replace(toRemoveChar, '_');
+            i = safeName.IndexOfAny(URI_ILLEGAL_CHARS, i);
+        }
+        return safeName;
+    }
+
+    static private string ReplaceInvalidCharactersFromFilePath(string input)
+    {
+        string sanitizedInput = input;
+
+        // Replace invalid characters with underscores
+        foreach (char c in Path.GetInvalidPathChars())
+        {
+            sanitizedInput = sanitizedInput.Replace(c, '_');
+        }
+
+        return sanitizedInput;
+    }
+    static private bool ContainsPathTraversalSequence(string path)
+    {
+        // Check for common path traversal sequences
+        return path.Contains("..") || path.Contains("%2e%2e") || path.Contains("/..") || path.Contains("\\..");
+    }
+}
+
+[Flags]
+public enum IllegalCharacterScrubOptions
+{
+    ScrubOBIllegalChars = 1,
+    ScrubWindowsIllegalChars = 2,
+    ScrubURIIllegalCharacters = 4,
+    All = ScrubOBIllegalChars | ScrubWindowsIllegalChars | ScrubURIIllegalCharacters,
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Test cases
+        string filePath = @"C:\Users\somaji\Desktop\..\web.config";
+
+        try
+        {           
+            string safeFileName = FileAndPathNameIllegalCharUtility.CreateSafeFileName(filePath, out string sanitizedFileName, IllegalCharacterScrubOptions.All);
+            Console.WriteLine($"Safe File Name: {safeFileName}");
+
+            // Create a safe file path
+            string safeFilePath = FileAndPathNameIllegalCharUtility.CreateSafeFilePath(filePath);
+            Console.WriteLine($"Safe File Path: {safeFilePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
